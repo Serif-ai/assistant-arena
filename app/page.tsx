d7f70af5b375902152cfe59cf7af98963dc35736
luke <luke@serif.ai>
2024-10-30 14:18:17 +0800

    add database config

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,14 @@ import prisma from "../lib/prisma";
 import { Button } from "@/components/ui/button";
 
 export default async function Home() {
-  const posts = await prisma.post.findMany();
+  const users = await prisma.user.findMany();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Button>Click me</Button>
       <div>
-        {posts.map((post) => (
-          <div key={post.id}>
-            <div>{post.title}</div>
-            <div>{post.content}</div>
+        {users.map((user) => (
+          <div key={user.id}>
+            <div>{user.ip}</div>
           </div>
         ))}
       </div>
